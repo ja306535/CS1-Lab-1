3e15f2906c3d40a3fbd9ef849ce51c932886a3a7
Jalil <151500208+ja306535@users.noreply.github.com>
2025-01-08 18:57:49 +0000

    ..

--- main.c
+++ main.c
@@ -1,10 +1,21 @@
+/*
+questions to ask:
+1.In the read data funciton do you use a pointer for the number of students because that value is not known yet? 
+2.Why in getMAxAverageStudent you dont use a pointer? is it because by then the value for the number of students is known and passed from readData?
+3.In the read data funciton why after you scan in the number of students you derefernce in the for loop?
+4.In getMaxAverageStudent what does int i represent?
+5.Can you help me set up c lion 
+6.help turning in the assignment 
+*/
+
+
 #include <stdio.h>
 #define MAXSIZE 500
 #include "main.h"
 
-int main () {
+int main() {
 
-struct Student students[MAXSIZE]; //declaring a student array struct 
+struct Student students[MAXSIZE]; //declaring a student array struct with a max size of 500
 
 int n; //number of students
 int i; //student id
@@ -19,36 +30,38 @@ return 0;
 
 }
 
-struct Student getMaxAverageStudent(struct Student *s, int n){
+struct Student getMaxAverageStudent(struct Student *s, int n){ //struct function that takes in a pointer to the student array and the number of students
 
-    int i;
-    int maxIdx=0; //index cointaining the student with the highest average 
+    int i; //variable to store highest average position?
+    
+    int maxIdx=0; //variable index cointaining the student with the highest average 
 
     float maxAvg=0; //highest average
 
-    for(i=0;i<n;i++){
-        if(s[i].average > maxAvg){
-            maxAvg=s[i].average;
-            maxIdx=i;
+    for(i=0;i<n;i++){ //for loop that iterates through the array to find the highest average 
+        if(s[i].average > maxAvg){ //if student average at index 0 is greater than current max average 
+            maxAvg=s[i].average; //the new average is set 
+            maxIdx=i; //the students position with the highest average is stored
 
         }
     }
-return s[maxIdx];
+return s[maxIdx]; //return the index where the highest average is 
 
 }
 
-void readData(struct Student *students, int *c){
+void readData(struct Student *students, int *c){ //function that takes in a pointer to the student array and a pointer to the number of students
 
-    scanf("%d",c);
+    scanf("%d",c); //scan in the number of students
 
-    for(int i=0; i<*c;i++){
-        scanf("%d",&students[i].student_ID);
+    for(int i=0; i<*c;i++){ // for loop that iterates through the number of students 
         
-        scanf("%d %d %d",&students[i].g1,&students[i].g2,&students[i].g3);
+        scanf("%d",&students[i].student_ID); //for each student scan in their id
         
-        students[i].average=(students[i].g1+students[i].g2+students[i].g3)/3.0;
+        scanf("%d %d %d",&students[i].g1,&students[i].g2,&students[i].g3); //scan in the 3 different grades for each student
+        
+        students[i].average=(students[i].g1+students[i].g2+students[i].g3)/3.0; //find the average of the grades for each student
 
-        printf(" %d %d %d %d %2f\n", students[i].student_ID, students[i].g1, students[i].g2, students[i].g3 students[i].average)
+        printf(" %d %d %d %d %2f\n", students[i].student_ID, students[i].g1, students[i].g2, students[i].g3 students[i].average) //print all information 
 
     }
 }
